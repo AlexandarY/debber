d39df6ac9e04c709594c0c809a6994caae4dfea5
Alexandar Y <alexyov@protonmail.com>
2025-08-10 14:04:50 +0300

    add initial commands for tool

--- cmd/debber/main.go
+++ cmd/debber/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "debber",
+	Short: "Simple debian package generator",
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+}
+
+var newCmd = &cobra.Command{
+	Use:   "new",
+	Short: "Generate a new debian.toml template",
+	Long:  `Generate a new debian.toml file with all required fields for a package build`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Fprintf(os.Stdout, "Generating debian.toml")
+	},
+}
+
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Create a debian/ directory from debian.toml",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Fprintf(os.Stdout, "Creating the debian/ dir and content")
+	},
+}
 
 func main() {
-	fmt.Println("Hello, debber!")
+	rootCmd.AddCommand(newCmd)
+	rootCmd.AddCommand(createCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %s", err)
+	}
 }
